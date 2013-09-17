9cedc4cfc38fc825b1e0754dad4e421a8f13793b
Anwar Haq <anwarhaq.us@gmail.com>
2013-09-17 04:33:35 -0500

    Added main file for Project1

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>CSCI E-15 Project # 1</title>
+</head>
+<body style="background-color: #dddddd">
+    <div>
+    <h2>CSCI E-15 Project # 1 </h2>
+	<?php
+echo "Hellow World!!!";
+?>
+ </div>
+
+</body>
+</html>
